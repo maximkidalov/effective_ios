34a2a4847a6ad5cba8dad9ae075206c200167e59
maximkidalov <117586406+maximkidalov@users.noreply.github.com>
2023-11-23 17:46:56 +0600

    добавил асеты

---
+++ Assets/Radius.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct Radius {
+    static let small: CGFloat = 10
+    static let medium: CGFloat = 12
+    static let large: CGFloat = 100
+}
---
+++ Assets/Sizes.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct Sizes {
+    static let frameSize: CGFloat = 56
+    static let headerSize: CGFloat = 327
+    static let iconSize: CGFloat = 16
+    static let avatarSize: CGFloat = 24
+    static let without: CGFloat = 0
+    static let bigAvatarSize: CGFloat = 48
+    static let smallAvatarSize: CGFloat = 20
+    static let navSize: CGFloat = 375
+    static let linewidth: CGFloat = 1
+    static let customFloat: CGFloat = 0.15
+    static let shadowSize: CGFloat = 295
+    static let customX: CGFloat = 2
+    static let customY: CGFloat = 12
+    static let customFloat2: CGFloat = 0.04
+    static let customFloat3: CGFloat = 0.10
+    static let idk: CGFloat = 65
+    static let customSize: CGFloat = 80
+}
